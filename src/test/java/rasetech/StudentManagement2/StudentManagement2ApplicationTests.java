851744ff464b409155645cf008f32a248f471c75
Owner <l2407844029@gmail.com>
2025-03-06 18:39:43 +0900

    初回

---
+++ src/test/java/rasetech/StudentManagement2/StudentManagement2ApplicationTests.java
@@ -0,0 +1,13 @@
+package rasetech.StudentManagement2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagement2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
