66325722b707c0934d3783befc5e7fbcb63d76bc
Owner <l2407844029@gmail.com>
2025-03-09 22:44:31 +0900

    Standard第5回　GETとPOST

--- src/main/java/rasetech/StudentManagement/StudentManagement2Application.java
+++ src/main/java/rasetech/StudentManagement/StudentManagement2Application.java
@@ -11,15 +11,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StudentManagement2Application {
 
+	private String name = "Enami Kouji";
+	private String age = "37";
+
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagement2Application.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String hello() {
-		return "Hello, World!";
+	@GetMapping("/studentInfo")
+	public String getStudentInfo() {
+		return name + " " + age + "sai";
 	}
+
 	//GET POST
 	//GETは取得する、リクエストの結果を受け取る
 	//POSTは情報を与える、渡す
+	@PostMapping("/studentInfo")
+	public void setStudentInfo(String name, String age){//setName=名前登録する
+		this.name = name;
+		this.age = age;
+	}
+	@PostMapping("/studentName")
+	public void updateStudentName(String name){
+		this.name = name;
+	}
 }
