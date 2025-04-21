7250ad0233d68f81a80e127ed776b65f3ae8f6e7
Ringo450 <appointment3310@gmail.com>
2025-04-21 18:52:57 +0900

    Standard第10回　Mysqlで作った受講生情報を、Intellijのターミナルへ連携済み。full_Name（アンダーバーの後は頭文字大文字）に注意！

--- src/main/java/rasetech/StudentManagement/StudentRepository.java
+++ src/main/java/rasetech/StudentManagement/StudentRepository.java
@@ -1,16 +1,14 @@
 package rasetech.StudentManagement;
 
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
+import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface StudentRepository {
 
-  @Select("SELECT * FROM student WHERE name = #{name}")
-  Student searchByName(String name);
+  @Select("SELECT * FROM students")
+  List<Student> search();//searchでリファクタリングして、シグネチャーの変更で「String name」消した。
 
   //@Insert、@Update、@Deleteも、第10回では使わないので、ノイズが起きないように消しておく。
 }
