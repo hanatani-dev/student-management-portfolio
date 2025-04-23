01f758a594646572e544e3680ce083705fd5f6e7
Ringo450 <appointment3310@gmail.com>
2025-04-23 14:05:11 +0900

    Standard第10回　Mysqlで作った受講生コース情報を、Intellijのターミナルへ連携済み。myaqlで作成した名称は動画と合わせる！

--- src/main/java/rasetech/StudentManagement/StudentRepository.java
+++ src/main/java/rasetech/StudentManagement/StudentRepository.java
@@ -7,8 +7,11 @@ import org.apache.ibatis.annotations.Select;
 @Mapper
 public interface StudentRepository {
 
-  @Select("SELECT * FROM students")
-  List<Student> search();//searchでリファクタリングして、シグネチャーの変更で「String name」消した。
-
   //@Insert、@Update、@Deleteも、第10回では使わないので、ノイズが起きないように消しておく。
+
+  @Select("SELECT * FROM student")
+  List<Student> searchStudent();//searchでリファクタリングして、シグネチャーの変更で「String name」消した。
+
+  @Select("SELECT * FROM courses")
+  List<StudentsCourses> searchStudentsCourses();
 }
