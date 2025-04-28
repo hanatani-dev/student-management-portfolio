a9e9479085dbd0ff27124f994a49b0f6ab766df0
Ringo450 <appointment3310@gmail.com>
2025-04-28 15:32:38 +0900

    Fix conflict: merge StudentController and add StudentDetail

--- src/main/java/rasetech/StudentManagement/Controller/StudentController.java
+++ src/main/java/rasetech/StudentManagement/Controller/StudentController.java
@@ -1,10 +1,14 @@
 package rasetech.StudentManagement.Controller;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import rasetech.Domain.StudentDetail;
 import rasetech.StudentManagement.Controller.Converter.StudentConverter;
+import rasetech.StudentManagement.Data.Student;
 import rasetech.StudentManagement.Data.StudentsCourses;
 import rasetech.StudentManagement.Service.StudentService;
 
@@ -23,11 +27,29 @@ public class StudentController {
     this.converter = converter;
   }
 
-  @GetMapping("/studentsList")
-  public List<StudentDetails> getStudentList() {
-    //リクエストの加工処理、入力チェックとか入ってくる。
-    return service.searchStudentList();
-    //これは Service層（ビジネスロジック層） のメソッドを呼んでる。コントローラーから呼び出すなら基本はService層を通すのがベスト！　直接Repositoryを呼ぶと、テスト・変更・拡張がしにくくなることがあるよ。
+  @GetMapping("/studentList")
+  public List<StudentDetail> getStudentList() {
+    List<Student> students = service.searchStudentList();
+    List<StudentsCourses> studentsCourses = service.searchStudentsCoursesList();
+
+    return converter.converterStudentDetails(students, studentsCourses);
+  }
+
+  private List<StudentDetail> convertStudentDetails(List<Student> students,
+      List<StudentsCourses> studentsCourses) {
+    List<StudentDetail> studentDetails = new ArrayList<>();
+    students.forEach(student -> {
+      StudentDetail studentDetail = new StudentDetail();
+      studentDetail.setStudent(student);
+
+      List<StudentsCourses> convertStudentCourses = studentsCourses.stream()
+          .filter(studentCourse -> student.getId().equals(studentCourse.getStudentId()))
+          .collect(Collectors.toList());//全件検索を絞りこむため。
+
+      studentDetail.setStudentsCourses(convertStudentCourses);
+      studentDetails.add(studentDetail);
+    });
+    return studentDetails;
   }
 
   @GetMapping("/studentsCourseList")
