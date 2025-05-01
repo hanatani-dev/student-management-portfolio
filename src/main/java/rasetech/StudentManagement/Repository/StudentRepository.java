318b28a76e50460cdb9a9da01554c4a4e1e4497c
Ringo450 <appointment3310@gmail.com>
2025-05-01 18:32:09 +0900

    受講生情報・コース情報ともに、更新作業もできるようRepositoryオブジェクトに追加

--- src/main/java/rasetech/StudentManagement/Repository/StudentRepository.java
+++ src/main/java/rasetech/StudentManagement/Repository/StudentRepository.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import rasetech.StudentManagement.Data.Student;
 import rasetech.StudentManagement.Data.StudentsCourses;
 
@@ -35,7 +36,18 @@ public interface StudentRepository {
   void registerStudent(Student student);
 
   // コース情報の保存
-  @Insert("INSERT INTO students_courses(student_id, course_name, start_data_at, end_data_at) " +
-      "VALUES(#{studentId}, #{courseName}, #{startDataAt}, #{endDataAt})")
+  @Insert("INSERT INTO students_courses(student_id, course_name, start_data_at, end_data_at) "
+      + "VALUES(#{studentId}, #{courseName}, #{startDataAt}, #{endDataAt})")
+  @Options(useGeneratedKeys = true, keyProperty = "id")
   void registerStudentsCourse(StudentsCourses course);
+
+  //学生情報の更新
+  @Update(
+      "UPDATE students SET(name=#{name}, nickname=#{nickname},"
+          + " email=#{email}, area=#{area}, age=#{age}, sex=#{sex}, remark=#{remark}, is_deleted=#{isDeleted}) WHERE id = #{id}")
+  void updateStudent(Student student);
+
+  // コース情報の更新
+  @Update("UPDATE student_courses SET(course_name = #{courseName})) WHERE id = #{id}")
+  void updateStudentsCourses(StudentsCourses studentsCourses);
 }
