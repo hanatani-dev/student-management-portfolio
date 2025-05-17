588aeb41fccfde278f166f3d764a507a2f5a6b58
Ringo450 <appointment3310@gmail.com>
2025-05-17 19:40:27 +0900

    動画と違うエラー出たため、GPTで解消した。：Repository.xmlのresultTypeに追加rasetech.StudentManagement.Data.Student。ただ、ファイル名が動画と異なっており、この文も異なっている。application.propertiesのclasspath文は、*1個抜いた。StudentManagement2Application.java(=Mainオブジェクト）に、@MapperScan追加。

--- src/main/java/rasetech/StudentManagement/StudentManagement2Application.java
+++ src/main/java/rasetech/StudentManagement/StudentManagement2Application.java
@@ -1,15 +1,18 @@
 package rasetech.StudentManagement;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("rasetech.StudentManagement.Repository")
 public class StudentManagement2Application {
 
   public static void main(String[] args) {
     SpringApplication.run(StudentManagement2Application.class, args);
   }
 }
+
 /**
  * Controllerのオブジェクトと、Serviceのオブジェクトを作って、 このメインオブジェクトで使ってた機能を移したので、すごく見やすくなった！
  */
