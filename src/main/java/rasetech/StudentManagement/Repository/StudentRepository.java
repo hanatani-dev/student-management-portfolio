10cd91ff9f8cef0e41851eca191b56944f556f86
Ringo450 <appointment3310@gmail.com>
2025-05-18 15:19:41 +0900

    第22回-XMLにSQL書けた。なのでRepository.java側のアノテーション（@Select・@Insert・@Update）を削除し、SQLを呼び出すメソッドの定義だけ残した。

--- src/main/java/rasetech/StudentManagement/Repository/StudentRepository.java
+++ src/main/java/rasetech/StudentManagement/Repository/StudentRepository.java
@@ -1,95 +1,37 @@
 package rasetech.StudentManagement.Repository;
 
 import java.util.List;
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Options;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
 import rasetech.StudentManagement.Data.Student;
 import rasetech.StudentManagement.Data.StudentCourse;
 
 /**
  * 受講生テーブルと受講生コース情報テーブルと紐づくRepositoryです。
  */
-
 @Mapper
 public interface StudentRepository {
 
-  /**
-   * 受講生の全件検索を行います。
-   *
-   * @return　受講生一覧（全件）
-   */
+  // 全受講生の取得
   List<Student> search();
 
-  /**
-   * 受講生の検索を行います。
-   *
-   * @param id 　受講生ID
-   * @return　受講生
-   */
+  // IDで受講生を検索
   Student searchStudent(String id);
 
-  /**
-   * 受講生のコース情報の全件検索を行います。
-   *
-   * @return　受講生のコース情報（全件）
-   */
-
-  @Select("SELECT * FROM students_courses")
+  // 全受講生コース情報の取得
   List<StudentCourse> searchStudentCourseList();
 
-  /**
-   * 受講生Idに紐づく受講生コース情報を検索します。
-   *
-   * @param stdentId 　受講生ID
-   * @return　受講生IDに紐づく受講生コース情報
-   */
-  @Select("SELECT * FROM students_courses WHERE student_id = #{studentId}")
-  List<StudentCourse> searchStudentCourse(String stdentId);
+  // 受講生IDに紐づくコース情報取得
+  List<StudentCourse> searchStudentCourse(String studentId);
 
-  /**
-   * 受講生を新規登録します。 IDに関しては自動採番を行う。
-   *
-   * @param student 　受講生
-   */
-
-  // 学生情報の保存
-  @Insert(
-      "INSERT INTO students(name,nickname, email, area, age, sex,remark,is_deleted)"
-          + " VALUES(#{name}, #{nickname}, #{email}, #{area}, #{age}, #{sex}, #{remark}, false)")
-  @Options(useGeneratedKeys = true, keyProperty = "id")
+  // 受講生の新規登録
   void registerStudent(Student student);
 
-  /**
-   * 受講生コース情報を新規登録します。 IDに関しては自動採番を行う。
-   *
-   * @param studentCourse 　受講生コース情報
-   */
-
-  @Insert("INSERT INTO students_courses(student_id, course_name, course_start_at, course_end_at) "
-      + "VALUES(#{studentId}, #{courseName}, #{courseStartAt}, #{courseEndAt})")
-  @Options(useGeneratedKeys = true, keyProperty = "id")
+  // コース情報の新規登録
   void registerStudentCourse(StudentCourse studentCourse);
 
-  /**
-   * 受講生を更新します。
-   *
-   * @param student 　受講生
-   */
-
-  @Update("UPDATE students SET name = #{name}, nickname = #{nickname},"
-      + "email = #{email}, area = #{area}, age = #{age}, sex = #{sex}, remark = #{remark}, is_deleted = #{isDeleted} WHERE id = #{id}")
+  // 受講生情報の更新
   void updateStudent(Student student);
 
-  /**
-   * 受講生コース情報のコース名を更新します。
-   *
-   * @param studentCourse 　受講生コース情報
-   */
-  @Update("UPDATE students_courses SET course_name = #{courseName} WHERE id = #{id}")
+  // コース名の更新（1件）
   void updateStudentCourse(StudentCourse studentCourse);
-  //WHERE id・・・ひとつのコース名だけ、変更するってこと。
-  // もしWHERE studentID＝とかにした場合：２つコース受けてて、ブラウザ更新画面でコース名Javaにしたら、受講してる２つのコース名とも、Javaになってしまう。
 }
