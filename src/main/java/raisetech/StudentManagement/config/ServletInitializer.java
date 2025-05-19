0706f8c6d72bdb5a0757102e6160dcfa589673a1
Ringo450 <appointment3310@gmail.com>
2025-05-19 14:52:22 +0900

    第22回-Mainクラスが2つあったため、古い方削除。Initializarをconfigパッケージに移動。Postmanで動作確認済。

--- src/main/java/raisetech/StudentManagement/config/ServletInitializer.java
+++ src/main/java/raisetech/StudentManagement/config/ServletInitializer.java
@@ -1,13 +1,15 @@
-package raisetech.StudentManagement2;
+package raisetech.StudentManagement.config;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import raisetech.StudentManagement.StudentManagementApplication;
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
   @Override
   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-    return application.sources(StudentManagement2Application.class);
+    // 修正後バージョン（新しいクラス名に合わせて）
+    return application.sources(StudentManagementApplication.class);
   }
 
 }
