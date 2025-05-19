aa6fa8b258d93b11edf5d9e151ec511f1af056b2
Ringo450 <appointment3310@gmail.com>
2025-05-19 13:36:45 +0900

    第22回-StudentCourseクラスにバリデーション追加。Postmanの全件検索でBindingExceptionエラー出たので、受講生全件検索select id=search追加。

--- src/main/java/rasetech/StudentManagement/Data/StudentCourse.java
+++ src/main/java/rasetech/StudentManagement/Data/StudentCourse.java
@@ -1,18 +1,30 @@
 package rasetech.StudentManagement.Data;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-
 public class StudentCourse {
 
+  @NotBlank(message = "コースIDは必須です。")
+  @Size(max = 10, message = "コースIDは10文字以内で入力してください。")
   private String id;
-  private String studentId;
-  private String courseName;
-  private LocalDateTime courseStartAt;
-  private LocalDateTime courseEndAt;
 
+  @NotBlank(message = "受講生IDは必須です。")
+  private String studentId;
+
+  @NotBlank(message = "コース名は必須です。")
+  @Size(max = 50, message = "コース名は50文字以内で入力してください。")
+  private String courseName;
+
+  @NotNull(message = "コース開始日は必須です。")
+  private LocalDateTime courseStartAt;
+
+  @NotNull(message = "コース終了日は必須です。")
+  private LocalDateTime courseEndAt;
 }
